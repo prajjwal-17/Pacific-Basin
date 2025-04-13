1195ff8cf939d15c9a1560380e13c9ac834d6cff
Abhi03057 <162039025+Abhi03057@users.noreply.github.com>
2025-04-14 02:12:49 +0530

    bottombar added

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React from "react";
+import "../styles/BottomBar.css";
 
 function BottomBar() {
   return (
-    <footer className="bg-gray-800 text-white p-3 text-center mt-auto">
-      © 2025 Prajjwal Rawat
+    <footer className="bottom-footer">
+      <img src="/logo.png" alt="Pacific Basin Logo" className="logo" />
+
+      <div className="subscribe-box">
+        <span className="subscribe-text">Subscribe for updates</span>
+        <div className="subscribe-icons">
+          <a href="https://www.facebook.com/pacificbasin.shipping/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.linkedin.com/company/pacificbasinshipping/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://x.com/pacificbasin" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-x-twitter"></i> {/* Fixed the icon class */}
+          </a>
+          <a href="https://www.youtube.com/channel/UCSdCGsQ6Vr98foRBEkv0gQA" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-youtube"></i>
+          </a>
+        </div>
+      </div>
+
+      <div className="footer-links">
+        <span>Sitemap</span>
+        <span>FAQ</span>
+        <span>Privacy</span>
+        <span>Terms</span>
+      </div>
+
+      <div className="copyright">
+        &copy; 2025 Pacific Basin Shipping Limited
+      </div>
     </footer>
   );
 }
